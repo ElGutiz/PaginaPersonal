89e4279b657de03bee731e655c52c886239ccbef
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-05-30 21:44:55 -0600

    Project Base

---
+++ src/simpleComponent/startApp.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class StartApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div className="startName">
+        <h1>Buenas</h1>
+      </div>
+    );
+  }
+}
+
+export default StartApp;
